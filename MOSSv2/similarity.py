671c1af522e71509efe65ba29b72080775a066b4
Aniket Joshi <aniket7joshi@gmail.com>
2020-04-27 01:55:48 +0530

    similaritites added and integrated

--- MOSSv2/similarity.py
+++ MOSSv2/similarity.py
@@ -5,7 +5,7 @@ from gensim import similarities
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.cluster import SpectralClustering
 import os
-
+import spacy
 
 def read_files(file_list):
 	documents = []
@@ -41,23 +41,58 @@ def check_gensim_similarity(file_list):
 	corpus = [dictionary.doc2bow(text) for text in texts]
 
 	lsi = models.LsiModel(corpus, id2word=dictionary, num_topics=4)
-	doc = documents[0]
-	vec_bow = dictionary.doc2bow(doc.lower().split())
-	vec_lsi = lsi[vec_bow]
+	allsimilarities = []
+	for k in range(len(documents)):
+	# print(len(documents))
+		similarity_particular = []
+		doc = documents[k]
+		vec_bow = dictionary.doc2bow(doc.lower().split())
+		vec_lsi = lsi[vec_bow]
+		index = similarities.MatrixSimilarity(lsi[corpus])
+		sims = index[vec_lsi]
+		# sims = sorted(enumerate(sims), key=lambda item: -item[1])
+		for i, s in enumerate(sims):
+			similarity_particular.append(s)
+		allsimilarities.append(similarity_particular)
+	return allsimilarities
+def read_files1(file_list):
+	documents = []
+	stoplist = set('for a of the and to in'.split())
+	for file in file_list:
+		filepath = './tests/' + file
+		contents = ""
+		with open(filepath) as f:
+			for line in f.readlines():
+				contents += line
+		file_content = str(contents)
+		documents.append(file_content)
+	texts = [
+		[word for word in document.lower().split() if word not in stoplist]
+		for document in documents
+	]
 
-	index = similarities.MatrixSimilarity(lsi[corpus])
-
-	sims = index[vec_lsi]
-	sims = sorted(enumerate(sims), key=lambda item: -item[1])
-	for i, s in enumerate(sims):
-		print(s, documents[s[0]])
+	return documents,texts
+def spacy_similarity(file_list):
+	nlp = spacy.load('en_core_web_sm')
+	documents,_= read_files1(file_list)
+	similarity_particular = []
+	for k in range(len(documents)):
+		doc = documents[k]
+		doc = nlp(doc)
+		similarity_a = []
+		for i in range(len(documents)):
+			doc_a = nlp(documents[i])
+			similarity_a.append(doc.similarity(doc_a))
+		similarity_particular.append(similarity_a)
+	return similarity_particular			
 
 
 def tf_idf_cosine_distance(file_list):
-	documents,_ = read_files(file_list)
+	documents,_ = read_files1(file_list)
 	tfidf = TfidfVectorizer().fit_transform(documents)
 	pairwise_similarity = tfidf * tfidf.T
-	print(pairwise_similarity)
+	# print(pairwise_similarity.toarray())
+	return pairwise_similarity.toarray()
 	no_of_files = len(file_list)
 	cluster_documents(pairwise_similarity,no_of_files)
 
@@ -65,4 +100,4 @@ def cluster_documents(pairwise_similarity,no_of_files):
 
 	scmodel = SpectralClustering(n_clusters=no_of_files - 1 , affinity='precomputed')
 	clusters = scmodel.fit_predict(pairwise_similarity)
-	print(clusters)
+	# print(clusters)
--- MOSSv2/views.py
+++ MOSSv2/views.py
@@ -5,88 +5,106 @@ from .forms import UserForm, UserProfileInfoForm
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
 from . import similarity
-
+import numpy as np
 def index(request):
-    return render(request, "MOSSv2/index.html", {"title": "Home"})
+	return render(request, "MOSSv2/index.html", {"title": "Home"})
 
 
 @login_required
 def special(request):
-    return HttpResponse("You are logged in!")
+	return HttpResponse("You are logged in!")
 
 
 @login_required
 def user_logout(request):
-    logout(request)
-    return HttpResponseRedirect(reverse("moss:index"))
+	logout(request)
+	return HttpResponseRedirect(reverse("moss:index"))
 
 @login_required
 def upload(request):
-    return render(request, "MOSSv2/upload.html", {"title": "Upload"})
+	return render(request, "MOSSv2/upload.html", {"title": "Upload"})
 
 
 def register(request):
-    registered = False
-    if request.method == "POST":
-        user_form = UserForm(data=request.POST)
-        profile_form = UserProfileInfoForm(data=request.POST)
-        if user_form.is_valid() and profile_form.is_valid():
-            user = user_form.save()
-            user.set_password(user.password)
-            user.save()
-            profile = profile_form.save(commit=False)
-            profile.user = user
-            if "profile_pic" in request.FILES:
-                print("found profile picture")
-                profile.profile_pic = request.FILES["profile_pic"]
-            profile.save()
-            registered = True
-        else:
-            print(user_form.errors, profile_form.errors)
+	registered = False
+	if request.method == "POST":
+		user_form = UserForm(data=request.POST)
+		profile_form = UserProfileInfoForm(data=request.POST)
+		if user_form.is_valid() and profile_form.is_valid():
+			user = user_form.save()
+			user.set_password(user.password)
+			user.save()
+			profile = profile_form.save(commit=False)
+			profile.user = user
+			if "profile_pic" in request.FILES:
+				print("found profile picture")
+				profile.profile_pic = request.FILES["profile_pic"]
+			profile.save()
+			registered = True
+		else:
+			print(user_form.errors, profile_form.errors)
 
-    else:
-        user_form = UserForm()
-        profile_form = UserProfileInfoForm()
-    return render(
-        request,
-        "MOSSv2/registration.html",
-        {
-            "user_form": user_form,
-            "profile_form": profile_form,
-            "registered": registered,
-            "title": "Register",
-        },
-    )
+	else:
+		user_form = UserForm()
+		profile_form = UserProfileInfoForm()
+	return render(
+		request,
+		"MOSSv2/registration.html",
+		{
+			"user_form": user_form,
+			"profile_form": profile_form,
+			"registered": registered,
+			"title": "Register",
+		},
+	)
 
 
 def user_login(request):
-    if request.method == "POST":
-        username = request.POST.get("username")
-        password = request.POST.get("password")
-        user = authenticate(username=username, password=password)
-        if user:
-            if user.is_active:
-                login(request, user)
-                return HttpResponseRedirect(reverse("moss:user_login"))
-            else:
-                return HttpResponse("Your account was inactive.")
-        else:
-            print("Invalid username: {} or password: {}".format(username, password))
-            return render(request, "MOSSv2/login.html", {"username":username, 
-            "password":password,
-            "title": "Login",
-            "error_msg" : "Invalid login details"})
-    else:
-        return render(request, "MOSSv2/login.html", {"title": "Login"})
+	if request.method == "POST":
+		username = request.POST.get("username")
+		password = request.POST.get("password")
+		user = authenticate(username=username, password=password)
+		if user:
+			if user.is_active:
+				login(request, user)
+				return HttpResponseRedirect(reverse("moss:user_login"))
+			else:
+				return HttpResponse("Your account was inactive.")
+		else:
+			print("Invalid username: {} or password: {}".format(username, password))
+			return render(request, "MOSSv2/login.html", {"username":username, 
+			"password":password,
+			"title": "Login",
+			"error_msg" : "Invalid login details"})
+	else:
+		return render(request, "MOSSv2/login.html", {"title": "Login"})
 
 @login_required
 def upload_files(request):
-    if request.method == "POST":
-        file_list = [x.name for x in request.FILES.getlist('file')]
-        files = request.FILES.getlist('file')
-        print("{} files received.".format(len(file_list)))
-        # gensim_similarity_matrix = similarity.check_gensim_similarity(files)
-        tf_idf_matrix = similarity.tf_idf_cosine_distance(files)
-    return render(request, "MOSSv2/upload.html", {"msg":"Files Uploaded",
-    "title": "Upload",
-    "file_list": file_list})
+	if request.method == "POST":
+		file_list = [x.name for x in request.FILES.getlist('file')]
+		files = request.FILES.getlist('file')
+		print("{} files received.".format(len(file_list)))
+		print(files)
+		gensim_similarity_matrix = similarity.check_gensim_similarity(files)
+		# contents = ""
+		# with open('./tests/' + file_list[0]) as f:
+		# 	for line in f.readlines():
+		# 		contents += line
+		print(np.array(gensim_similarity_matrix))
+		# print(contents)
+		# print('*****')
+		# contents = ""
+		# with open('./tests/' + file_list[1]) as f:
+		# 	for line in f.readlines():
+		# 		contents += line
+		# print(contents)
+		# print('*****')
+		# print(files)
+		tf_idf_matrix = similarity.tf_idf_cosine_distance(file_list)
+		print(tf_idf_matrix)
+		spacy_similarity = similarity.spacy_similarity(file_list)
+		print(np.array(spacy_similarity))
+	return render(request, "MOSSv2/upload.html", {"msg":"Files Uploaded",
+	"title": "Upload",
+	"file_list": file_list})
